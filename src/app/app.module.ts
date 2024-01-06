d16fe1f2f9d2afaccff655e58f4a089c9a1fadec
Lucho Cuevas <lcuevas@be-flow.com>
2024-01-06 17:35:21 -0400

    Fin-seccion-4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <h3>Counter: {{ counter }}</h3>
+    <button class="btn btn-secondary me-2" (click)="increaseBy(1)">+1</button>
+    <button class="btn btn-secondary me-2" (click)="resetCounter()">
+      Reset
+    </button>
+    <button class="btn btn-secondary me-2" (click)="increaseBy(-1)">-1</button>
+  `,
+})
+export class CounterComponent {
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
+  constructor() {}
+}
